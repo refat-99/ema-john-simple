d943483d5d048ddd7cde83e9114f764d20d6fba0
Md Refadul Islam <refadulislam99@gmail.com>
2023-01-19 20:43:15 +0600

    change someting

--- src/components/Invantory/Invantory.js
+++ src/components/Invantory/Invantory.js
@@ -4,6 +4,7 @@ const Invantory = () => {
     return (
         <div>
             <h2>This is Invantory</h2>
+            <h3>The Section Created for Invantory Information</h3>
         </div>
     )
 };
